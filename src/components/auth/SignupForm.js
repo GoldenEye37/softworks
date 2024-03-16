a5b2e307b2810cc257163e8b245c49f3fc6d0166
Leon Dhimba <dhimbaleon356@gmail.com>
2024-03-16 17:03:09 +0200

    feat: register form

--- src/components/auth/SignupForm.js
+++ src/components/auth/SignupForm.js
@@ -3,7 +3,7 @@ import { signup } from '../../services/authService';
 import {set} from "lodash";
 
 // font awesome icons
-import {faCheck} from "@fortawesome/free-solid-svg-icons";
+import {faCheck, faInfo, faInfoCircle, faTimes} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 // regex for validating our data
@@ -15,7 +15,12 @@ const PHONE_REGEX = /^\d{10}$/;
 const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 const SignupForm = () => {
-    const userRef = useRef();
+    const nameRef = useRef();
+    const lastNameRef = useRef();
+    const countryIdRef  = useRef();
+    const phoneRef  = useRef();
+    const emailRef  = useRef();
+
     const errRef = useRef();
 
     // use useState to store data
@@ -144,51 +149,254 @@ const SignupForm = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input
-                type="text"
-                name="fname"
-                value={formData.fname}
-                onChange={handleChange}
-                placeholder="First Name"
-            />
-            <input
-                type="text"
-                name="lname"
-                value={formData.lname}
-                onChange={handleChange}
-                placeholder="Last Name"
-            />
-            <input
-                type="number"
-                name="country_id"
-                value={formData.country_id}
-                onChange={handleChange}
-                placeholder="Country ID"
-            />
-            <input
-                type="number"
-                name="phone"
-                value={formData.phone}
-                onChange={handleChange}
-                placeholder="Phone Number"
-            />
-            <input
-                type="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                placeholder="Email"
-            />
-            <input
-                type="password"
-                name="password"
-                value={formData.password}
-                onChange={handleChange}
-                placeholder="Password"
-            />
-            <button type="submit">Signup</button>
-        </form>
+        <section onSubmit={handleSubmit}>
+            <p
+                ref={errRef}
+                className={errMsg ? "errmsg" : "offscreen"}
+                aria-live="assertive"
+            >
+                {errMsg}
+            </p>
+            <form>
+                <label
+                    htmlFor="firstName"
+                >
+                    First Name:
+                    <span
+                        className={validFirstName ? "valid" : "hide"}
+                    >
+                        <FontAwesomeIcon icon={faCheck}/>
+                    </span>
+                    <span
+                        className={validFirstName || !firstName ? "hide" : "valid"}
+                    >
+                        <FontAwesomeIcon icon={faTimes}/>
+                    </span>
+                </label>
+                <input
+                    type="text"
+                    name="firstName"
+                    ref={nameRef}
+                    autoComplete="off"
+                    onChange={handleChange} // todo
+                    required
+                    aria-invalid={validFirstName ? "false" : "true"}
+                    aria-describedby="uidnote"
+                    onFocus={() => setFirstNameFocus(true)}
+                    onBlur={() => setFirstNameFocus(false)}
+                    placeholder="example: John"
+                />
+                <p
+                    id="uidnote"
+                    className={firstNameFocus && firstName && !validFirstName ? "instructions" : "offscreen"}
+                >
+                    <FontAwesomeIcon icon={faInfoCircle}/>
+                    First Name must have 4 to 24 characters.
+                </p>
+
+
+                {/**********************last Name*******************/}
+                <label
+                    htmlFor="lastName"
+                >
+                    Last Name:
+                    <span
+                        className={validLastName ? "valid" : "hide"}
+                    >
+                        <FontAwesomeIcon icon={faCheck}/>
+                    </span>
+                    <span
+                        className={validLastName || !lastName ? "hide" : "valid"}
+                    >
+                        <FontAwesomeIcon icon={faTimes}/>
+                    </span>
+                </label>
+                <input
+                    type="text"
+                    name="lastName"
+                    ref={lastNameRef}
+                    autoComplete="off"
+                    onChange={handleChange} // todo
+                    required
+                    aria-invalid={validFirstName ? "false" : "true"}
+                    aria-describedby="uidnote"
+                    onFocus={() => setLastNameFocus(true)}
+                    onBlur={() => setLastNameFocus(false)}
+                    placeholder="example: John Smith"
+                />
+                <p
+                    id="uidnote"
+                    className={lastNameFocus && lastName && !validLastName ? "instructions" : "offscreen"}
+                >
+                    <FontAwesomeIcon icon={faInfoCircle}/>
+                    Last Name must have 4 to 24 characters.
+                </p>
+
+
+                {/**********************Country Id*******************/}
+                <label
+                    htmlFor="country_id"
+                >
+                    Country ID:
+                    <span
+                        className={validCountryId ? "valid" : "hide"}
+                    >
+                        <FontAwesomeIcon icon={faCheck}/>
+                    </span>
+                    <span
+                        className={validCountryId || !countryId ? "hide" : "valid"}
+                    >
+                        <FontAwesomeIcon icon={faTimes}/>
+                    </span>
+                </label>
+                <input
+                    type="text"
+                    name="countryId"
+                    // ref={lastNameRef}
+                    onChange={handleChange} // todo
+                    required
+                    aria-invalid={validCountryId ? "false" : "true"}
+                    aria-describedby="uidnote"
+                    onFocus={() => setCountryIdFocus(true)}
+                    onBlur={() => setCountryIdFocus(false)}
+                />
+
+
+                {/**********************Phone Number*******************/}
+                <label
+                    htmlFor="country_id"
+                >
+                    Phone Number:
+                    <span
+                        className={validPhoneNumber ? "valid" : "hide"}
+                    >
+                        <FontAwesomeIcon icon={faCheck}/>
+                    </span>
+                    <span
+                        className={validPhoneNumber || !phoneNumber ? "hide" : "valid"}
+                    >
+                        <FontAwesomeIcon icon={faTimes}/>
+                    </span>
+                </label>
+                <input
+                    type="text"
+                    name="phoneNumber"
+                    // ref={lastNameRef}
+                    onChange={handleChange} // todo
+                    required
+                    aria-invalid={validPhoneNumber ? "false" : "true"}
+                    aria-describedby="uidnote"
+                    onFocus={() => setPhoneNumberFocus(true)}
+                    onBlur={() => setPhoneNumberFocus(false)}
+                />
+
+
+                {/**********************Email Address*******************/}
+                <label
+                    htmlFor="country_id"
+                >
+                    Email Address:
+                    <span
+                        className={validEmailAddress ? "valid" : "hide"}
+                    >
+                        <FontAwesomeIcon icon={faCheck}/>
+                    </span>
+                    <span
+                        className={validEmailAddress || !emailAddress ? "hide" : "valid"}
+                    >
+                        <FontAwesomeIcon icon={faTimes}/>
+                    </span>
+                </label>
+                <input
+                    type="email"
+                    name="emailAddress"
+                    // ref={lastNameRef}
+                    onChange={handleChange} // todo
+                    required
+                    aria-invalid={validPhoneNumber ? "false" : "true"}
+                    aria-describedby="uidnote"
+                    onFocus={() => setPhoneNumberFocus(true)}
+                    onBlur={() => setPhoneNumberFocus(false)}
+                />
+
+                {/**********************Password*******************/}
+                <label
+                    htmlFor="password"
+                >
+                    Password:
+                    <FontAwesomeIcon
+                        icon={faCheck}
+                        className={validPassword ? "valid" : "hide"}
+                    />
+                    <FontAwesomeIcon
+                        icon={faTimes}
+                        className={validPassword || !password ? "hide" : "invalid"}/>
+                </label>
+                <input
+                    type="password"
+                    id="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    required
+                    aria-invalid={validPassword ? "false" : "true"}
+                    aria-describedby="passwordnote"
+                    onFocus={() => setpasswordFocus(true)}
+                    onBlur={() => setpasswordFocus(false)}
+                />
+                <p
+                    id="passwordnote"
+                    className={passwordFocus && !validPassword ? "instructions" : "offscreen"}
+                >
+                    <FontAwesomeIcon icon={faInfoCircle}/>
+                    8 to 24 characters.<br/>
+                    Must include uppercase and lowercase letters, a number and a special character.<br/>
+                    Allowed special characters: <span aria-label="exclamation mark">!</span> <span
+                    aria-label="at symbol">@</span> <span aria-label="hashtag">#</span> <span
+                    aria-label="dollar sign">$</span> <span aria-label="percent">%</span>
+                </p>
+
+                {/**********************Confirm Password*******************/}
+
+                <label htmlFor="confirm_password">
+                    Confirm Password:
+                    <FontAwesomeIcon icon={faCheck} className={validPasswordMatch && matchPassword ? "valid" : "hide"}/>
+                    <FontAwesomeIcon icon={faTimes}
+                                     className={validPasswordMatch || !matchPassword ? "hide" : "invalid"}/>
+                </label>
+                <input
+                    type="password"
+                    id="confirm_password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={matchPassword}
+                    required
+                    aria-invalid={validPasswordMatch ? "false" : "true"}
+                    aria-describedby="confirmnote"
+                    onFocus={() => setMatchPasswordFocus(true)}
+                    onBlur={() => setMatchPasswordFocus(false)}
+                />
+                <p id="confirmnote"
+                   className={matchPasswordFocus && !validPasswordMatch ? "instructions" : "offscreen"}>
+                    <FontAwesomeIcon icon={faInfoCircle}/>
+                    Must match the first password input field.
+                </p>
+
+                <button disabled={
+                    !validFirstName ||
+                    !validPassword ||
+                    !validPasswordMatch
+                }>
+                    Sign Up
+                </button>
+            </form>
+
+            <p>
+                Already registered?<br/>
+                <span className="line">
+                   {/*put router link here*/}
+                    <a href="#">Sign In</a>
+                        </span>
+            </p>
+        </section>
     )
 
 };
